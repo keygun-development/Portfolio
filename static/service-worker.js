1884a49cb59047e79abd77a75029d00f6bab547c
Keagan <keygun2k1@gmail.com>
2023-05-30 14:42:31 +0200

    Added service worker

---
+++ static/service-worker.js
@@ -0,0 +1,21 @@
+import {precacheAndRoute} from 'workbox-precaching';
+import {setCatchHandler} from 'workbox-routing';
+
+self.addEventListener('install', event => {
+    const files = ['/offline.html']; // you can add more resources here
+    event.waitUntil(
+        self.caches.open('offline-fallbacks')
+            .then(cache => cache.addAll(files))
+    );
+});
+
+setCatchHandler(async (options) => {
+    const destination = options.request.destination;
+    const cache = await self.caches.open('offline-fallbacks');
+    if (destination === 'document') {
+        return (await cache.match('/offline.html')) || Response.error();
+    }
+    return Response.error();
+});
+
+precacheAndRoute(self.__WB_MANIFEST);
