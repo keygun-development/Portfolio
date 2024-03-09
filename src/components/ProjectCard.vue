f93d348a124c37ccbcbbdf3af6b171b65afe124f
Keagan <keygun2k1@gmail.com>
2024-03-09 01:19:17 +0100

    Only show when image can be fetched

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -22,7 +22,7 @@ fetchImage(props.project.name)
 </script>
 
 <template>
-  <a target="_blank" :href="project.homepage ? project.homepage : project.html_url" class="grid-item">
+  <a v-if="link" target="_blank" :href="project.homepage ? project.homepage : project.html_url" class="grid-item">
     <img class="aspect-video object-cover w-full rounded-t-xl" :src="link"/>
     <div class="p-8 bg-gray-800 rounded-b-xl">
       <h3 class="sm:text-2xl text-xl font-bold text-orange-400 capitalize">
